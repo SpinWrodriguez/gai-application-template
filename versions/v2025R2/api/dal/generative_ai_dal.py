9d4c7b741ebf24491f5b9a647c0a85017e3f236e
William Rodriguez <wrodriguez@spinifexit.com>
2025-07-09 14:26:18 +1000

    SDK AI Python done

--- versions/v2025R2/api/dal/generative_ai_dal.py
+++ versions/v2025R2/api/dal/generative_ai_dal.py
@@ -1,12 +1,8 @@
 import os
-import json
-import requests
 from dotenv import load_dotenv
 from middleware.auth import get_sap_token
+from gen_ai_hub.proxy.native.openai import chat
 
-load_dotenv()
-
-API_URL = os.getenv("SAP_GAI_API_URL")
 RESOURCE_GROUP = "default"
 
 system_prompt = """
@@ -17,29 +13,11 @@ def generate_text_from_sap(user_input: str, model="gpt-4o", extra_settings=None)
     if extra_settings is None:
         extra_settings = {}
 
-    payload = {
-        "model": model,
-        "messages": [
-            { "role": "system", "content": system_prompt },
-            { "role": "user", "content": user_input }
-        ],
-        **extra_settings
-    }
+    messages = [
+        {  "role": "system", "content": system_prompt},
+        {  "role": "user", "content": user_input }
+    ]
 
-    access_token = get_sap_token()
-
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-        "Content-Type": "application/json",
-        "AI-Resource-Group": RESOURCE_GROUP
-    }
-
-    response = requests.post(API_URL, headers=headers, json=payload)
-
-    if response.status_code != 200:
-        raise Exception(f"Failed to get AI response: {response.status_code} - {response.text}")
-
-    print("🔹Prompt Used:", json.dumps(payload, indent=2))
-
-    # Return just the message content
-    return response.json()["choices"][0]["message"]["content"]
+    kwargs = dict(deployment_id='d9d3d64879df0b88', messages=messages)
+    response = chat.completions.create(**kwargs)
+    return (response.choices[0].message.content)
