b1229d988f40b2fff0154d2d8bc776cda675d626
William Rodriguez <wrodriguez@spinifexit.com>
2025-07-09 15:57:18 +1000

    AI stream implemented

--- versions/v2025R2/api/dal/generative_ai_dal.py
+++ versions/v2025R2/api/dal/generative_ai_dal.py
@@ -6,7 +6,7 @@ from gen_ai_hub.proxy.native.openai import chat
 RESOURCE_GROUP = "default"
 
 system_prompt = """
-Your Personal Training Assistant is here to help you with your fitness journey.
+Your Personal Training Assistant is here to help you with your fitness journey, keep it short and consice.
 """
 
 def generate_text_from_sap(user_input: str, model="gpt-4o", extra_settings=None) -> str:
@@ -14,10 +14,29 @@ def generate_text_from_sap(user_input: str, model="gpt-4o", extra_settings=None)
         extra_settings = {}
 
     messages = [
-        {  "role": "system", "content": system_prompt},
-        {  "role": "user", "content": user_input }
+        { "role": "system", "content": system_prompt },
+        { "role": "user", "content": user_input }
     ]
 
-    kwargs = dict(deployment_id='d9d3d64879df0b88', messages=messages)
-    response = chat.completions.create(**kwargs)
-    return (response.choices[0].message.content)
+    kwargs = dict(
+        deployment_id='d9d3d64879df0b88',
+        messages=messages,
+        max_tokens=500,
+        temperature=0.7,
+        top_p=0.6,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stream=True,
+    )
+
+    stream = chat.completions.create(**kwargs)
+
+    full_response = ""
+    for chunk in stream:
+        if not chunk.choices:
+            continue
+        delta = chunk.choices[0].delta
+        print (chunk.choices[0].delta)
+        if delta and isinstance(delta.content, str):
+            full_response += delta.content
+    return full_response
